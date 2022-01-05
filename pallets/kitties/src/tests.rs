9b8e5bd5fb7ffa46b9c81136d4fa026454d63052
aaron@zt Manjaro <rootisalone@outlook.com>
2022-01-05 22:43:39 +0800

    first setup for mock

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -1,1 +1,13 @@
+use crate::{mock::*, Error};
+use frame_support::{assert_noop, assert_ok};
+
+#[test]
+fn it_works_for_create_a_kitty() {
+    new_test_ext().execute_with(|| {
+        // Dispatch a signed extrinsic.
+        assert_noop!(KittiesModule::create_kitty(Origin::signed(1)), Error::<Test>::NotEnoughBalance);
+        // Read pallet storage and assert an expected result.
+    });
+}
+
 
